aba66d95db80875578fb32b6afbd3b863e11ce58
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-09-19 16:29:16 +0700

    Install socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { app } from "./app";
+import http from "http";
+import {Server} from "socket.io";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server  = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', () => {
+    console.log('New Websocket connection');
+})
+
+server.listen(port, () => {
     console.log('Listening on port 3000');
 });
