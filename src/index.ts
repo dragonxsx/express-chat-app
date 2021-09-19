5aa3f46a2d4c8abe1f1dec7ae35b6f974cf97d67
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-09-19 17:57:30 +0700

    Experiment with the socket events

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,18 @@ const port = process.env.PORT || 3000;
 const server  = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', () => {
+let count = 0;
+
+io.on('connection', (socket) => {
     console.log('New Websocket connection');
-})
+
+    socket.emit('countUpdated', count);
+
+    socket.on('increment', () => {
+        count++;
+        io.emit('countUpdated', count);
+    })
+});
 
 server.listen(port, () => {
     console.log('Listening on port 3000');
